4ae247c2e75011d6852eddb5103e9d2285d42f3c
kriss <462679766@qq.com>
2017-08-11 16:53:06 +0800

    feat: lanIp 可以设置域名

--- Applications/app/config.php
+++ Applications/app/config.php
@@ -10,6 +10,16 @@ if (!function_exists('env')) {
     }
 }
 
+if (!function_exists('host2Ip')) {
+    function host2Ip($host)
+    {
+        if(filter_var($host, FILTER_VALIDATE_IP)){
+            return $host;
+        }
+        return gethostbyname($host);
+    }
+}
+
 $config = [
     // 服务注册地址
     'registerAddress' => env('REGISTER_ADDRESS', '127.0.0.1:1238'),
@@ -26,7 +36,7 @@ $config = [
     // gateway 数量,等于CPU数
     'gatewayCount' => env('GATEWAY_COUNT', 2),
     // 本机ip，分布式部署时使用内网ip
-    'gatewayLanIp' => env('LAN_IP', '127.0.0.1'),
+    'gatewayLanIp' => host2Ip(env('LAN_IP', '127.0.0.1')),
     // 内部通讯起始端口，假如$gateway->count=4，起始端口为4000
     // 则一般会使用4000 4001 4002 4003 4个端口作为内部通讯端口
     'gatewayStartPort' => 4000,
